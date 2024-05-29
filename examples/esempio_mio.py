e079bb56403a6195d994e73ef3c329bcd45e8154
Andrew Spiers <48754526+DrAndrewSpiers@users.noreply.github.com>
2024-05-29 09:31:23 +0100

    test from Susi

---
+++ examples/esempio_mio.py
@@ -0,0 +1,141 @@
+import sys
+import os
+print("begin")
+
+import matplotlib.pyplot as plt
+#matplotlib inline
+import numpy as np
+
+from few.trajectory.inspiral import EMRIInspiral
+from few.amplitude.romannet import RomanAmplitude
+from few.amplitude.interp2dcubicspline import Interp2DAmplitude
+from few.waveform import FastSchwarzschildEccentricFlux, SlowSchwarzschildEccentricFlux, GenerateEMRIWaveform
+from few.utils.utility import (get_overlap, 
+                               get_mismatch, 
+                               get_fundamental_frequencies, 
+                               get_separatrix, 
+                               get_mu_at_t, 
+                               get_p_at_t, 
+                               get_kerr_geo_constants_of_motion,
+                               xI_to_Y,
+                               Y_to_xI)
+
+from few.utils.ylm import GetYlms
+from few.utils.modeselector import ModeSelector
+from few.summation.interpolatedmodesum import CubicSplineInterpolant
+from few.waveform import SchwarzschildEccentricWaveformBase
+from few.summation.interpolatedmodesum import InterpolatedModeSum
+from few.summation.directmodesum import DirectModeSum
+from few.utils.constants import *
+from few.summation.aakwave import AAKSummation
+from few.waveform import Pn5AAKWaveform, AAKWaveformBase
+
+use_gpu = False
+
+# keyword arguments for inspiral generator (RunSchwarzEccFluxInspiral)
+inspiral_kwargs={
+        "DENSE_STEPPING": 0,  # we want a sparsely sampled trajectory
+        "max_init_len": int(1e3),  # all of the trajectories will be well under len = 1000
+    }
+
+# keyword arguments for inspiral generator (RomanAmplitude)
+amplitude_kwargs = {
+    "max_init_len": int(1e3),  # all of the trajectories will be well under len = 1000
+    "use_gpu": use_gpu  # GPU is available in this class
+}
+
+# keyword arguments for Ylm generator (GetYlms)
+Ylm_kwargs = {
+    "assume_positive_m": False  # if we assume positive m, it will generate negative m for all m>0
+}
+
+# keyword arguments for summation generator (InterpolatedModeSum)
+sum_kwargs = {
+    "use_gpu": use_gpu,  # GPU is availabel for this type of summation
+    "pad_output": False,
+}
+
+
+few = FastSchwarzschildEccentricFlux(
+    inspiral_kwargs=inspiral_kwargs,
+    amplitude_kwargs=amplitude_kwargs,
+    Ylm_kwargs=Ylm_kwargs,
+    sum_kwargs=sum_kwargs,
+    use_gpu=use_gpu,
+)
+
+
+gen_wave = GenerateEMRIWaveform("Pn5AAKWaveform")
+
+# parameters
+T = 0.1  # years
+dt = 15.0  # seconds
+M = 1e6
+a = 0.9 
+mu = 1e1
+p0 = 12.0
+e0 = 0.2
+x0 = 0.9  # notice this is x_I, not Y. The AAK waveform can convert to Y. 
+qK = 0.2  # polar spin angle
+phiK = 0.2  # azimuthal viewing angle
+qS = 0.3  # polar sky angle
+phiS = 0.3  # azimuthal viewing angle
+dist = 1.0  # distance
+Phi_phi0 = 1.0
+Phi_theta0 = 2.0
+Phi_r0 = 3.0
+
+#h = gen_wave(
+#    M,
+#    mu,
+#    a,
+#   p0,
+#    e0,
+#    x0,
+#    dist,
+#    qS,
+#    phiS,
+#    qK,
+#    phiK,
+#    Phi_phi0,
+#    Phi_theta0,
+#    Phi_r0,
+#    T=T,
+#    dt=dt,
+#)
+
+#plt.plot(h.real[:2000])
+#plt.show()
+
+#print(h.real[1])
+#print(h.real[10])
+#print(h.real[len(h.real)-1])
+
+few = FastSchwarzschildEccentricFlux(
+    inspiral_kwargs=inspiral_kwargs,
+    amplitude_kwargs=amplitude_kwargs,
+    Ylm_kwargs=Ylm_kwargs,
+    sum_kwargs=sum_kwargs,
+    use_gpu=use_gpu,
+)
+
+M = 5e0
+mu = 1e-4
+print(mu)
+p0 = 12.0
+e0 = 0.4
+theta = np.pi/3  # polar viewing angle
+phi = np.pi/4  # azimuthal viewing angle
+dt = 10.0
+
+wave = few(M, mu, p0, e0, theta, phi, dt=dt, T=0.1)  #  assumes dt = 10.0 for max T = 1.0 year
+
+t = np.arange(len(wave))*dt
+plt.plot(t, wave.real)
+plt.plot(t, wave.imag)
+
+dt*(len(wave) - 1)
+
+plt.show()
+
+print("end")
